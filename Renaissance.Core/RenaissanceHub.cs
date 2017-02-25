707a19f42d50baeb1f02ebbd82688e3c79afdcff
Andrey Drobyshev <andrey.drobyschev@gmail.com>
2017-02-25 17:08:39 +0400

    Added Startup and RenaissanceHub

---
+++ Renaissance.Core/RenaissanceHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace Renaissance.Core
+{
+    public class RenaissanceHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+    }
+}
---
+++ Renaissance.Core/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Renaissance.Core.Startup))]
+namespace Renaissance.Core
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
